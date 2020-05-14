611759131ed1115d95c5bea2e7775b379c78aaf4
hxiaohua <811836659@qq.com>
2020-05-14 13:14:10 +0800

    2020

--- A筛选班级和考场.py
+++ A筛选班级和考场.py
@@ -1,6 +1,7 @@
 #coding:utf-8
 import openpyxl
 from datetime import datetime
+from openpyxl.styles import Border,Side
 
 #基础设置
 file_name="AAA.xlsx"#文件名字
@@ -8,7 +9,6 @@ Every_kc=3	#几个考场换行
 kao_name="高二年级5月考试考场安排"
 
 print("开始筛选数据")
-
 #设置班级或者考场的最大人数
 max_class=80
 max_kc=45
@@ -43,6 +43,12 @@ while a<rows:
 print(Class_num[0])
 print(KaoC_num[0])
 
+#设置边框
+border = Border(left=Side(border_style='thin',color='000000'),
+			right=Side(border_style='thin',color='000000'),
+			top=Side(border_style='thin',color='000000'),
+			bottom=Side(border_style='thin',color='000000'))
+
 #搜索所有记录，将对应班级和考场复制到相应区域
 i=2#标记行号，从第二行开始计算
 while i<=rows:
@@ -63,6 +69,11 @@ while i<=rows:
 	sht_bj.cell(row+1,bj_ys*5-3).value=ws.cell(i,2).value
 	sht_bj.cell(row+1,bj_ys*5-2).value=ws.cell(i,3).value#继续处理考场
 	sht_bj.cell(row+1,bj_ys*5-1).value=ws.cell(i,4).value
+	#添加边框
+	sht_bj.cell(row+1,bj_ys*5-4).border=border
+	sht_bj.cell(row+1,bj_ys*5-3).border=border
+	sht_bj.cell(row+1,bj_ys*5-2).border=border
+	sht_bj.cell(row+1,bj_ys*5-1).border=border
 	
 	kc_value= ws.cell(i,3).value
 	kc_value=int(kc_value)
@@ -73,23 +84,27 @@ while i<=rows:
 		kc_s=kc_s-1
 		kc_ys=4
 	row=int(kc_s*max_kc+KaoC_num[kc_value])
-	#print(bj_ys*5)
+	#复制过去
 	sht_kc.cell(row+1,kc_ys*5-4).value=ws.cell(i,1).value
 	sht_kc.cell(row+1,kc_ys*5-3).value=ws.cell(i,2).value
 	sht_kc.cell(row+1,kc_ys*5-2).value=ws.cell(i,3).value
 	sht_kc.cell(row+1,kc_ys*5-1).value=ws.cell(i,4).value
-	
-	#sht_kc= wb.create_sheet("按考场")
-	#第三列是考场
-	#print(kc_vale)
+	#添加边框
+	sht_kc.cell(row+1,kc_ys*5-4).border=border
+	sht_kc.cell(row+1,kc_ys*5-3).border=border
+	sht_kc.cell(row+1,kc_ys*5-2).border=border
+	sht_kc.cell(row+1,kc_ys*5-1).border=border
+
 	kc_value=int(kc_value)
 	#找到班级，将一行数据复制走，继续搜索
 	i=i+1
 
 i=1#标记行号，从第二行开始计算
+sum=0
 while i<=KaoC_num[0]:
 	#第i考场数据处理
     kc_value=i
+    sum=sum+KaoC_num[i]
     kc_s=kc_value//4
     kc_ys=kc_value%4
     if(kc_ys==0):
@@ -100,13 +115,20 @@ while i<=KaoC_num[0]:
     sht_kc.cell(row+1,kc_ys*5-3).value="班级"
     sht_kc.cell(row+1,kc_ys*5-2).value="考场"
     sht_kc.cell(row+1,kc_ys*5-1).value="考号"
-    print(KaoC_num[i])
+	#设置边框
+    sht_kc.cell(row+1,kc_ys*5-4).border=border
+    sht_kc.cell(row+1,kc_ys*5-3).border=border
+    sht_kc.cell(row+1,kc_ys*5-2).border=border
+    sht_kc.cell(row+1,kc_ys*5-1).border=border
+    #print(KaoC_num[i])
     i=i+1
-	
+print("按考场总数："+str(sum))
 i=1#班级标记
+sum=0
 while i<=Class_num[0]:
 	#第i考场数据处理
     kc_value=i
+    sum=sum+Class_num[i]
     kc_s=kc_value//4
     kc_ys=kc_value%4
     if(kc_ys==0):
@@ -117,10 +139,14 @@ while i<=Class_num[0]:
     sht_bj.cell(row+1,kc_ys*5-3).value="班级"
     sht_bj.cell(row+1,kc_ys*5-2).value="考场"
     sht_bj.cell(row+1,kc_ys*5-1).value="考号"
-    print(Class_num[i])
+    #边框
+    sht_bj.cell(row+1,kc_ys*5-4).border=border
+    sht_bj.cell(row+1,kc_ys*5-3).border=border
+    sht_bj.cell(row+1,kc_ys*5-2).border=border
+    sht_bj.cell(row+1,kc_ys*5-1).border=border
+    #print(Class_num[i])
     i=i+1
-
-
+print("按班级总数："+str(sum))
 #考场基本结束
 dt = datetime.now()
 #dt= dt.strftime( '%Y-%m-%d %H:%M:%S %f' )
@@ -129,7 +155,6 @@ dt= dt.strftime( '%Y%m%d %H_%M' )
 wb.save("AAABBB.xlsx")  # 保存
 #暂停程序，按键退出
 input("任意键盘结束")
-
 
 
 
